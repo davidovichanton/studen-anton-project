4a855ea16bccb150e0757196b010c634860a9ddb
Anton Davidovich <david20toxa@gmail.com>
2020-05-07 20:46:18 +0300

    problem with task4

---
+++ hometask8_github_task4.py
@@ -0,0 +1,17 @@
+Написать функцию которая принимает число и возвращает True если число
+положительное и делиться на два и False в противном случае
+>>> is_positive_and_even(3) == False
+>>> is_positive_and_even(42) == True
+>>> is_positive_and_even(-1) == False
+"""
+
+
+def is_positive_and_even(n: int) -> bool:
+    if n / 2 == :
+        return True
+
+    if n / 2 =! :
+        return False
+
+print(is_positive_and_even(432))
+            
