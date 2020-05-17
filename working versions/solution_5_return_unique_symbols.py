0bee228bfbd89df53cf16ee63739d0587f8eacd3
Anton Davidovich <david20toxa@gmail.com>
2020-05-17 23:02:56 +0300

    delete file solution_7_returns_a_list_of_items.py and add solution_5_return_unique_simbols

---
+++ working versions/solution_5_return_unique_symbols.py
@@ -0,0 +1,19 @@
+"""
+Написать функцию которая принимает строку и возвращает уникальные символы в
+этой строке
+>>> unique_symbols("aaab") == ["a", "b"]
+>>> unique_symbols("abc") == ["a", "b", "c"]
+"""
+from typing import List
+
+def unique_symbols_with_count(s):
+"""функция возращает уникальные символы в строке
+"""
+    seen = []
+    for i in s:
+        if i not in seen:
+            senn.append(i)
+        return seen
+
+
+print(unique_symbols_with_count("aaab"))
