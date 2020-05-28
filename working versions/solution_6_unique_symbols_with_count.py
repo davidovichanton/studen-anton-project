2d0c6a942211f1a2cfa98505cd045a82a42c6c0d
Anton Davidovich <david20toxa@gmail.com>
2020-05-28 20:07:17 +0300

    added comments

---
+++ working versions/solution_6_unique_symbols_with_count.py
@@ -0,0 +1,33 @@
+"""
+Написать функцию которая принимает строку и возвращает количество уникальные
+символы в этой строке и их количество
+>>> unique_symbols_with_count("aaab") == {"a": 3, "b": 1}
+>>> unique_symbols_with_count("abc") == {"a": 1, "b": 1, "c": 1}
+"""
+
+from typing import Dict
+
+def unique_symbols_with_count(s):
+    """Функция находит уникальный символ и сколько раз
+    он встретился в строке
+    """
+    result = {}
+    for symbol in s:
+        if symbol not in result:
+            count = count_symbol(s, symbol)
+            result[symbol] = count
+    return result
+
+def count_symbol(s, symbol):
+    """Функция считает сколько раз символ встречается
+        в переданной ее строке
+    """
+    count = 0
+    for i in s:
+        if i == symbol:
+            count += 1
+
+    return count
+
+print(unique_symbols_with_count("aaabb"))
+print(unique_symbols_with_count("acccc"))
