b751259cc6a0284dddd7f7503164fcbf503aaa81
Anton Davidovich <david20toxa@gmail.com>
2020-06-10 22:05:40 +0300

    first of task_9 solution

---
+++ at the decision stage/solution_9_max_digit.py
@@ -0,0 +1,18 @@
+"""
+Написать функцию которая принимает число и возвращает максимальную цифру из
+этого числа
+>>> max_digit(10)
+1
+>>> max_digit(41339)
+9
+>>> max_digit(658456000)
+8
+"""
+
+def max_digit(n):
+    result = []
+    for i in n:
+        result.append(i)
+    return max(result)
+
+print(max_digit(str(10)))
